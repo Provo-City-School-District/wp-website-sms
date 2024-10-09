baa3196c8552ed6e2d18d8b83e3b7915f6a47912
Josh Espinoza <joshespi@gmail.com>
2024-10-09 11:43:03 -0600

    process shortcodes in sidebar

--- wp-content/themes/shoreline-child/sidebar-about.php
+++ wp-content/themes/shoreline-child/sidebar-about.php
@@ -1,4 +1,7 @@
 <aside id="mainSidebar">
 	<h1>School Information</h1>
-	<?= get_post(18322)->post_content; ?>
+	<?php
+	$page_content = get_post(18322);
+	do_shortcode($page_content->post_content);
+	?>
 </aside>
